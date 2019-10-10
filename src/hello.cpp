252d4a845a9ccabe097987a3309b508a184d8b63
Jon Bøgetoft <Jbaget19@student.aau.dk>
2019-10-10 23:01:06 +0200

    Ask for name

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main(int argc, char const *argv[])
-{
-    std::cout << "Hello, World!" << std::endl;
+{ 
+    std::string name;
+    std::cout << "What's your name?: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << std::endl;
     return 0;
 }
